1366fff1956ffadcaa8f85438a3f70fcb0fb0268
Gregor von Laszewski <laszewski@gmail.com>
2020-03-26 12:32:56 -0400

    update broken man page

--- cloudmesh/cluster/command/cluster.py
+++ cloudmesh/cluster/command/cluster.py
@@ -18,103 +18,109 @@ class ClusterCommand(PluginCommand):
     # noinspection PyUnusedLocal
     @command
     def do_cluster(self, args, arguments):
+
+
+
+
         """
-                ::
+        ::
 
-                Usage:
-                        cluster test
-                        cluster create LABEL [--vms=NAMES... | --n=N]
-                                       [--cloud=CLOUD]
-                        cluster add [LABEL] --vms=NAMES...
-                        cluster remove [LABEL] --vms=NAMES...
-                        cluster terminate [LABEL] [--vms=NAMES...]
-                        cluster info [LABEL] [--verbose]
+          Usage:
+            cluster test
+            cluster create LABEL [--vms=NAMES... | --n=N]
+                           [--cloud=CLOUD]
+            cluster add [LABEL] --vms=NAMES...
+            cluster remove [LABEL] --vms=NAMES...
+            cluster terminate [LABEL] [--vms=NAMES...]
+            cluster info [LABEL] [--verbose]
 
-                This command allows you to create and interact with an available
-                cluster of machines.
+          This command allows you to create and interact with an available
+          cluster of machines.
 
-                Arguments:
-                    ID      An existing machine ID to be reserved for the cluster.
-                    LABEL   The label for the cluster.
-                    CLOUD   Cloud platform to initialize VMs.
-                    N       Number of instances to request [default: 5]
-                    V       Verbosity level.
+          Arguments:
+            ID      An existing machine ID to be reserved for the cluster.
+            LABEL   The label for the cluster.
+            CLOUD   Cloud platform to initialize VMs.
+            N       Number of instances to request [default: 5]
+            V       Verbosity level.
 
-                Options:
-                    --id       Specify string containing list of IDs, comma delimited
-                               format "id1,id2,...,idx".
-                    --cloud    Specify cloud platform {AWS, Azure, Openstack}.
-                    --n                Specify number of VMs to initialize.
-                    --all      OPTIONAL.  Overrides --id, will pass all machines as
-                               an argument.
-                    --verbose  OPTIONAL.  Provides verbosity level for info.
+          Options:
+            --id       Specify string containing list of IDs, comma delimited
+                       format "id1,id2,...,idx".
+            --cloud    Specify cloud platform {AWS, Azure, Openstack}.
+            --n                Specify number of VMs to initialize.
+            --all      OPTIONAL.  Overrides --id, will pass all machines as
+                       an argument.
+            --verbose  OPTIONAL.  Provides verbosity level for info.
 
-                Description:
+          Description:
 
-                    create:
+            create:
 
-                        if vms are specified use them
+                if vms are specified use them
 
-                        if -N is used create new vms and add them the the label
-                        cluster
+                if -N is used create new vms and add them the the label
+                cluster
 
-                        cloudmehs has a cm.group make it cm.gourp=LABEL
-                        when you start a vm
-
-
-                    add:
-
-                        adds the named vpma use Parameter.expand(arguments,vm)
+                cloudmehs has a cm.group make it cm.gourp=LABEL
+                when you start a vm
 
 
-                    remoiv terminate:
+            add:
 
-                        same as add
+                adds the named vpma use Parameter.expand(arguments,vm)
 
 
-            info:
+            remoiv terminate:
 
-                cool output with Printer.write
+                same as add
 
-                        cluster build --id=ID LABEL
 
-                                Groups together existing machines and reserves
-                                them for cluster use.  Pass a comma-delimited
-                                list of machine ID's as a string. Pass --all to
-                                associate all available machines to cluster.
+          info:
 
-                        cluster create --cloud=CLOUD --n=N LABEL
+            cool output with Printer.write
 
-                                Automatically requests VMs from the cloud
-                                service requested.
+          Description:
 
-                        cluster add --id="[ID]" --all LABEL
+            cluster build --id=ID LABEL
 
-                                Adds given machine IDs to cluster.  Pass --all
-                                to associate all available machines to cluster.
+                    Groups together existing machines and reserves
+                    them for cluster use.  Pass a comma-delimited
+                    list of machine ID's as a string. Pass --all to
+                    associate all available machines to cluster.
 
-                        cluster remove --id="[ID]" LABEL
+            cluster create --cloud=CLOUD --n=N LABEL
 
-                                Removes given machine IDs from cluster.  Pass
-                                --all to disassociate all machines from cluster.
+                    Automatically requests VMs from the cloud
+                    service requested.
 
-                        cluster terminate --all LABEL
+            cluster add --id="[ID]" --all LABEL
 
-                                Terminates all instances associated with the
-                                cluster, wipes cluster data.  If --all is
-                                passed, terminates all running clusters.
+                    Adds given machine IDs to cluster.  Pass --all
+                    to associate all available machines to cluster.
 
-                        cluster info --all --verbose=v [LABEL]
+            cluster remove --id="[ID]" LABEL
 
-                                Retrieves cluster data and machine data
-                                associatred with cluster.  Verbosity level 1
-                                provides high-level cluster information and list
-                                of machines associated.  Verbosity level 2
-                                provides cluster information, machine
-                                information and status. Verbosity level 3
-                                provides all available information.
+                    Removes given machine IDs from cluster.  Pass
+                    --all to disassociate all machines from cluster.
 
-                """
+            cluster terminate --all LABEL
+
+                    Terminates all instances associated with the
+                    cluster, wipes cluster data.  If --all is
+                    passed, terminates all running clusters.
+
+            cluster info --all --verbose=v [LABEL]
+
+                    Retrieves cluster data and machine data
+                    associatred with cluster.  Verbosity level 1
+                    provides high-level cluster information and list
+                    of machines associated.  Verbosity level 2
+                    provides cluster information, machine
+                    information and status. Verbosity level 3
+                    provides all available information.
+
+        """
         map_parameters(arguments,
                        'id',
                        'label',
